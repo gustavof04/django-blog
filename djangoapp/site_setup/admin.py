dabe7a398c07a3e9fcd322039e3669f47c049046
gustavof04 <fernandes.gustavo2910@gmail.com>
2023-11-06 13:01:12 -0400

    chore: Deleta MenuLinkAdmin pois a model MenuLink já possui um inline em SiteSetupAdmin

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -3,13 +3,6 @@ from django.http.request import HttpRequest
 from site_setup.models import MenuLink, SiteSetup
 
 
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
-    list_display = 'id', 'text', 'url_or_path',
-    list_display_links = 'id', 'text', 'url_or_path',
-    search_fields = 'id', 'text', 'url_or_path',
-
-
 class MenuLinkInline(admin.TabularInline):
     model = MenuLink
     extra = 1
