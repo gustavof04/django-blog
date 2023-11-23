3a8065b5e38c8c558b44804f6a9390cd38e2ae98
gustavof04 <fernandes.gustavo2910@gmail.com>
2023-11-23 17:54:19 -0400

    feat: Cria model Category

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -16,3 +16,19 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(
+        unique=True, default=None,
+        null=True, blank=True, max_length=255,
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.name, 4)
+        return super().save(*args, **kwargs)
