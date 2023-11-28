6fdbe9d318328bface0af76497233efe454f4fc1
gustavof04 <fernandes.gustavo2910@gmail.com>
2023-11-28 10:29:44 -0400

    feat: Cria model Page com sua admin

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -6,7 +6,7 @@ class Tag(models.Model):
         verbose_name = 'Tag'
         verbose_name_plural = 'Tags'
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=65)
     slug = models.SlugField(
         unique=True, default=None,
         null=True, blank=True, max_length=255,
@@ -16,13 +16,16 @@ class Tag(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=65)
     slug = models.SlugField(
         unique=True, default=None,
         null=True, blank=True, max_length=255,
@@ -32,3 +35,29 @@ class Category(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.name, 4)
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Page(models.Model):
+    title = models.CharField(max_length=65)
+    slug = models.SlugField(
+        unique=True, default="",
+        null=False, blank=True, max_length=255
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisará estar marcado '
+            'para a página ser exibida publicamente.'
+        ),
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_new(self.title, 4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
